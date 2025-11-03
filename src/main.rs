afd41b657206d17d050da44db9f6fb78994b56c4
yodsaphonh <66011212039@msu.ac.th>
2025-11-03 11:38:36 +0700

    Add tag autocomplete using TagComplete data

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, get_service},
+    Router,
+};
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::TraceLayer,
+};
+use tracing::{error, info};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Clone)]
+struct AppState {
+    static_dir: Arc<PathBuf>,
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let static_dir = Arc::new(PathBuf::from("static"));
+
+    let app_state = AppState {
+        static_dir: static_dir.clone(),
+    };
+
+    let static_files = get_service(
+        ServeDir::new(static_dir.as_ref())
+            .append_index_html_on_directories(true)
+            .fallback(ServeFile::new(static_dir.join("index.html"))),
+    )
+    .handle_error(|err| async move {
+        error!("static serve error: {err}");
+        (StatusCode::INTERNAL_SERVER_ERROR, "static error")
+    });
+
+    let app = Router::new()
+        .route("/", get(index))
+        .fallback_service(static_files)
+        .with_state(app_state)
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    info!("Listening on http://{addr}");
+
+    if let Err(err) = axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app).await {
+        error!("server error: {err}");
+    }
+}
+
+async fn index(State(state): State<AppState>) -> impl IntoResponse {
+    let index_path = state.static_dir.join("index.html");
+    match tokio::fs::read(&index_path).await {
+        Ok(contents) => Response::builder()
+            .status(StatusCode::OK)
+            .header("Content-Type", "text/html; charset=utf-8")
+            .body(contents.into())
+            .unwrap(),
+        Err(err) => {
+            error!("failed to read index.html: {err}");
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
+}
